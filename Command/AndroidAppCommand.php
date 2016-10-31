002f791448cc6e52cc7e7a6ead1cf86d8f897226
Benjamin Touchard <benjamin@kolapsis.com>
2016-10-31 15:36:02 +0100

    Generator options

--- Command/AndroidAppCommand.php
+++ Command/AndroidAppCommand.php
@@ -61,9 +61,16 @@ final class AndroidAppCommand extends ContainerAwareCommand {
             ->setDescription('Generate/update Android application from Bundle.')
             ->setHelp("Generating Android application with account and authentication, content providers and sync system all based on your entities defined in your bundle")
             ->addArgument('bundle', InputArgument::REQUIRED, 'Target bundle')
-            ->addOption('android', 'a', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'App Android version', [24, 21])
-            ->addOption('sdk', 's', InputOption::VALUE_REQUIRED, 'Android SDK path', '/opt/android-sdk')
-            ->addOption('gradle', 'g', InputOption::VALUE_REQUIRED, 'Gradle version', '2.2.0')
+            ->addOption('target_sdk', 't', InputOption::VALUE_REQUIRED, 'Android target SDK', 24)
+            ->addOption('min_sdk', 'm', InputOption::VALUE_REQUIRED, 'Android target SDK min version', 21)
+            ->addOption('sdk_path', 's', InputOption::VALUE_REQUIRED, 'Android SDK path', '/opt/android-sdk')
+            ->addOption('gradle_version', 'g', InputOption::VALUE_REQUIRED, 'Gradle version', '2.2.0')
+            ->addOption('app_name', 'a', InputOption::VALUE_REQUIRED, 'App name')
+            ->addOption('app_path', 'p', InputOption::VALUE_REQUIRED, 'App path')
+            ->addOption('app_domain', 'd', InputOption::VALUE_REQUIRED, 'App domain name')
+            ->addOption('app_package', 'k', InputOption::VALUE_REQUIRED, 'App package name')
+            ->addOption('api_url', 'u', InputOption::VALUE_REQUIRED, 'Api url')
+            ->addOption('no_build_confirm', 'y', InputOption::VALUE_REQUIRED, 'No build confirm', false)
         ;
     }
 
@@ -76,9 +83,16 @@ final class AndroidAppCommand extends ContainerAwareCommand {
         $this->output = $output;
         $bundleName = $input->getArgument('bundle');
 
-        list($androidVersion, $minSdkVersion) = $input->getOption('android');
-        $sdkPath = $input->getOption('sdk');
-        $gradleVersion = $input->getOption('gradle');
+        $androidVersion = $input->getOption('target_sdk');
+        $minSdkVersion = $input->getOption('min_sdk');
+        $sdkPath = $input->getOption('sdk_path');
+        $gradleVersion = $input->getOption('gradle_version');
+        $appName = $input->getOption('app_name');
+        $path = $input->getOption('app_path');
+        $domainName = $input->getOption('app_domain');
+        $packageName = $input->getOption('app_package');
+        $apiUrl = $input->getOption('api_url');
+        $build = $input->getOption('no_build_confirm') !== false;
 
         $helper = $this->getHelper('question');
         $kernel = $this->getContainer()->get('kernel');
@@ -88,8 +102,37 @@ final class AndroidAppCommand extends ContainerAwareCommand {
             exit;
         }
 
-        $skeletonDirs = $this->getSkeletonDirs($kernel, $bundle);
-        $this->twig = $this->getTwigEnvironment($skeletonDirs);
+        if (empty($appName)) {
+            $appName = str_replace('Bundle', '', $bundle->getName());
+            $question = new Question('Please enter the <info>App Name</info> [' . $appName . ']: ', $appName);
+            $appName = $helper->ask($input, $output, $question);
+        }
+
+        if (empty($path)) {
+            if (AndroidAppCommand::$DEBUG) $path = '/home/benjamin/Documents/workspace/' . $appName;
+            else $path = dirname($kernel->getRootDir()) . '/android/' . $appName;
+            $question = new Question('Please enter the <info>App path</info> [' . $path . ']: ', $path);
+            $path = $helper->ask($input, $output, $question);
+        }
+
+        if (empty($domainName)) {
+            $question = new Question('Please enter the <info>App domain name</info> [kolapsis.com]: ', 'kolapsis.com');
+            $domainName = $helper->ask($input, $output, $question);
+        }
+
+        if (empty($apiUrl)) {
+            if (AndroidAppCommand::$DEBUG) $apiUrl = 'http://192.168.0.28/FullApp/web/api';
+            else $apiUrl = 'http://' . preg_replace('/[\s_-]+/', '', strtolower($appName)) . '.' . $domainName;
+            $question = new Question('Please enter the <info>App API url</info> [' . $apiUrl . ']: ', $apiUrl);
+            $apiUrl = $helper->ask($input, $output, $question);
+        }
+
+        if (empty($packageName)) {
+            $tmp = explode('.', $domainName);
+            $tmp = array_reverse($tmp);
+            $packageName = implode('.', $tmp) . '.' . preg_replace('/[\s_-]+/', '', strtolower($appName));
+        }
+
 
         $output->writeln([
             '==================================',
@@ -98,37 +141,26 @@ final class AndroidAppCommand extends ContainerAwareCommand {
             '<info>Android API: '.$androidVersion.' (min: '.$minSdkVersion.')</info>',
             '<info>Android SDK path: '.$sdkPath.'</info>',
             '<info>Gradle: '.$gradleVersion.'</info>',
+            '----------------------------------',
+            '<info>App name: '.$appName.'</info>',
+            '<info>App path: '.$path.'</info>',
+            '<info>Package: '.$packageName.'</info>',
+            '<info>API url: '.$apiUrl.'</info>',
             '',
         ]);
 
-        $appName = str_replace('Bundle', '', $bundle->getName());
-        $question = new Question('Please enter the <info>App Name</info> ['.$appName.']: ', $appName);
-        $appName = $helper->ask($input, $output, $question);
+        if (!$build) {
+            $question = new Question('<question>This is correct ? [Y/n]</question>', 'Y');
+            $build = $helper->ask($input, $output, $question) === 'Y';
+        }
 
-        if (AndroidAppCommand::$DEBUG) $path = '/home/benjamin/Documents/workspace/' . $appName;
-        else $path = dirname($kernel->getRootDir()) . '/android';
+        if (!$build) {
+            $this->output->writeln('<comment>Bye</comment>');
+            exit;
+        }
 
-        $question = new Question('Please enter the <info>App final path</info> ['.$path.']: ', $path);
-        $path = $helper->ask($input, $output, $question);
-
-        $question = new Question('Please enter the <info>App domain name</info> [kolapsis.com]: ', 'kolapsis.com');
-        $domainName = $helper->ask($input, $output, $question);
-
-        if (AndroidAppCommand::$DEBUG) $apiUrl = 'http://192.168.0.28/FullApp/web/api';
-        else $apiUrl = 'http://' . preg_replace('/[\s_-]+/', '', strtolower($appName)) . '.' . $domainName;
-
-        $question = new Question('Please enter the <info>App API url</info> ['.$apiUrl.']: ', $apiUrl);
-        $apiUrl = $helper->ask($input, $output, $question);
-
-        $tmp = explode('.', $domainName);
-        $tmp = array_reverse($tmp);
-        $packageName = implode('.', $tmp) . '.' . preg_replace('/[\s_-]+/', '', strtolower($appName));
-
-        $output->writeln('==================================');
-        $output->writeln('');
-        $output->writeln('Generating App: "' . $appName . '" [package: ' . $packageName . ']');
-        $output->writeln('==================================');
-        $output->writeln('');
+        $skeletonDirs = $this->getSkeletonDirs($kernel, $bundle);
+        $this->twig = $this->getTwigEnvironment($skeletonDirs);
 
         $parser = new BundleParser($this->output, $this->getContainer());
         $parser->parse($bundle);
